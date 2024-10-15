0b4dc9c119b739826afd917d604331d80a2c65de
Minhyuk12 <sunyart12@gmail.com>
2024-10-15 12:14:52 +0900

    initial commit'

---
+++ src/main/java/itm/oss/Main.java
@@ -0,0 +1,13 @@
+package itm.oss;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
